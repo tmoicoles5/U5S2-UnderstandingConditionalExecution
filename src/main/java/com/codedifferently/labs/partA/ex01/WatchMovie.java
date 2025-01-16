12ac11810b7817b54823d1475c639c314b9bdaef
tmoicoles5 <colestmoi@gmail.com>
2025-01-16 18:56:23 -0500

    push java

--- src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
@@ -4,9 +4,18 @@ public class WatchMovie {
     public static String watchMovie() {
         String response ="";
         /* Your code goes here*/
+        int moviePrice = 12;
+        int movieRating = 5;
+        if (moviePrice >= movieRating) {
+            response += ("I'm interested in watching the movie");
+        }else {
+            response +=("I'm not interested in watching the movie");
+        }
 
 
-        response += ("Enter the movie ticket price \n");
+
+
+
         return response;
 
     }
